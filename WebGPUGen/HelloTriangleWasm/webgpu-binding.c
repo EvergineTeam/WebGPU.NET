bf5b597f17f2d1eb13d0fe59cd8d2e4492a98ab7
Marcos Cobeña Morián <mcobena@plainconcepts.com>
2025-05-28 13:21:30 +0200

    clean-up

--- WebGPUGen/HelloTriangleWasm/webgpu-binding.c
+++ WebGPUGen/HelloTriangleWasm/webgpu-binding.c
@@ -1,7 +1,6 @@
 #include "emscripten.h"
 #include "emscripten/html5.h"
 #include "emscripten/html5_webgpu.h"
-#include "webgpu/webgpu.h"
 
 WGPUDevice webgpu_binding_emscripten_webgpu_get_device() {
     return emscripten_webgpu_get_device();
