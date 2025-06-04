eabb7097f916817a7757b9b1b66779affada1097
Marcos Cobeña Morián <mcobena@plainconcepts.com>
2025-06-04 12:14:31 +0200

    move wgpu_native.c into Evergine.Bindings.WebGPU

---
+++ WebGPUGen/Evergine.Bindings.WebGPU/WebGPUNative.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Evergine.Bindings.WebGPU
+{
+    public static unsafe partial class WebGPUNative
+    {
+        [DllImport("wgpu_native")]
+        public static extern void emscripten_get_element_css_size(char* target, double* width, double* height);
+
+        [DllImport("wgpu_native")]
+        public static extern WGPUDevice emscripten_webgpu_get_device();
+    }
+}
---
+++ WebGPUGen/Evergine.Bindings.WebGPU/runtimes/browser-wasm/wgpu_native.c
@@ -0,0 +1,4 @@
+#include "emscripten.h"
+#include "emscripten/html5.h"
+#include "emscripten/html5_webgpu.h"
+#include <webgpu/webgpu.h>
